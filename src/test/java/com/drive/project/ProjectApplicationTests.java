50a5c72f76e53349fd9686d6bb455bd47484a09c
ach968 <andrewkkchen@gmail.com>
2024-08-15 15:21:49 -0700

    initialized spring project

---
+++ src/test/java/com/drive/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drive.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
